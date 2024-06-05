e44b208cc01046aa9349b3f83cf998ac18741696
nueng <wongsaokrn.kump@gmail.com>
2024-06-05 12:50:15 +0700

    feat:add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+let [inputText,clickText] = useState('')
+let toggle =(event)=>{
+  clickText(event)
+}
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{inputText}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={()=>toggle('สวัสดี!')}>สวัสดี!</button>
+        <button onClick={()=>toggle('Hi!')}>Hi!</button>
+        <button onClick={()=>toggle('你好!')}>你好!</button>
       </div>
     </div>
   );
