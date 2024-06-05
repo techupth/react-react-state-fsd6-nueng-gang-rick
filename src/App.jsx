f50a5734c5265632843ffeafb0e2122e888119ea
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-06-05 14:09:16 +0700

    feat: usetate for say hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingText, setGreeting] = useState("Greeting Message");
+
+  const sayHiFromThai = () => {
+    setGreeting("สวัสดี!");
+  };
+
+  const sayHiFromEng = () => {
+    setGreeting("Hi!");
+  };
+
+  const sayHiFromChai = () => {
+    setGreeting("你好!");
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingText}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={sayHiFromThai}>สวัสดี!</button>
+        <button onClick={sayHiFromEng}>Hi!</button>
+        <button onClick={sayHiFromChai}>你好!</button>
       </div>
     </div>
   );
